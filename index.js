eca963d680592ab44fdd8f66a1f89908e6aff0a6
pascal <pascal.nicintije@student.moringaschool.com>
2024-03-19 14:26:06 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const headQuarterBlockStreet = 42;
+function distanceFromHqInBlocks(location) {
+    return Math.abs(headQuarterBlockStreet - location)
+}
+
+function distanceFromHqInFeet(location) {
+    return distanceFromHqInBlocks(location) * 264
+}
+function distanceTravelledInFeet(startLocation, destinationLocation) {
+    return (Math.abs(destinationLocation - startLocation))*264
+}
+function calculatesFarePrice(startLocation, destinationLocation) {
+    let travelled = distanceTravelledInFeet(startLocation, destinationLocation)
+    if (travelled <= 400) {
+        return 0
+    }
+    else if (travelled > 400 && travelled <= 2000) {
+        return (travelled - 400) * 0.02
+    }
+    else if (travelled > 2000 && travelled <= 2500) {
+        return 25
+    }
+    else{
+        return "cannot travel that far"
+    }
+}   
